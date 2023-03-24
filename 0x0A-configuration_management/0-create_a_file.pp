cf9c1c9f3a59f5321f5ee1e3203be47af6662ef3
davidslayin <dave191994@gmail.com>
2023-03-24 15:55:34 -0700

    trying again

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-# creating a file in /tmp/school
+# create file named school in tmp with below specification
 
 file { '/tmp/school':
-    ensure  => file,
-    path    => '/tmp/school'.
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love puppet'
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
